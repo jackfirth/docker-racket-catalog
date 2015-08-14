f657e821f72e9dc48cd5b9f59e318a25388928d2
Jack Firth <jackhfirth@gmail.com>
2015-08-13 23:36:59 -0700

    Remove logging

--- client-app/src/services/packages-svc.js
+++ client-app/src/services/packages-svc.js
@@ -11,9 +11,7 @@ export default ($http) => {
   const getPackages = () => {
     return $http.get('/api/pkgs-all')
       .then(R.prop('data'))
-      .then(R.tap(v => console.log("Data: ", v)))
       .then(readRacket)
-      .then(R.tap(v => console.log("Catalog: ", v)))
       .then(R.values)
       .catch(logPackageSvcError);
   };
