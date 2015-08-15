0c84273e18bd66e268e6fce3471d1fccda833d4b
Jack Firth <jackhfirth@gmail.com>
2015-08-14 22:22:19 -0700

    Fix injections

--- client-app/src/services/index.js
+++ client-app/src/services/index.js
@@ -1,6 +1,8 @@
 import PackagesSvc from './packages-svc';
+import PublishSvc from './publish-svc';
 
 
 export default {
-  PackagesSvc: ['$http', PackagesSvc]
+  PackagesSvc: ['$http', PackagesSvc],
+  PublishSvc: ['PackagesSvc', PublishSvc]
 };
